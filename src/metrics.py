4c6464862689b716783981ac52b31cf1c0bbf059
Ivan Valles Perez <ivanvallesperez@gmail.com>
2021-02-28 23:49:41 +0000

    Fix crossentropy metric

--- src/metrics.py
+++ src/metrics.py
@@ -9,6 +9,7 @@ def accuracy(y_true, y_pred):
 
 
 def crossentropy(y_true, y_pred, eps=1e-9):
+    y_pred = np.exp(y_pred) / np.exp(y_pred).sum(axis=1, keepdims=True)
     mask = np.eye(y_pred.shape[1])[y_true]
     p = np.sum(y_pred * mask, axis=1)
     return -np.log(np.clip(p, eps, 1 - eps)).mean()
