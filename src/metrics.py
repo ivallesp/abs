e247c83e2ff6f3c59659019dea12b94876588c4a
Ivan Valles Perez <ivanvallesperez@gmail.com>
2021-03-01 01:58:20 +0000

    Use scipy softmax, in log space, for num stability

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import softmax
 
 
 def accuracy(y_true, y_pred):
@@ -9,7 +10,7 @@ def accuracy(y_true, y_pred):
 
 
 def crossentropy(y_true, y_pred, eps=1e-9):
-    y_pred = np.exp(y_pred) / np.exp(y_pred).sum(axis=1, keepdims=True)
+    y_pred = softmax(y_pred, axis=1)
     mask = np.eye(y_pred.shape[1])[y_true]
     p = np.sum(y_pred * mask, axis=1)
     return -np.log(np.clip(p, eps, 1 - eps)).mean()
