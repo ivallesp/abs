d3aade52437574318dca00c4828ffa4c870217ae
Ivan Valles Perez <ivanvallesperez@gmail.com>
2021-03-13 15:03:51 +0000

    Fix seed parametrization

--- batcher.sh
+++ batcher.sh
@@ -4,21 +4,21 @@ set -e
 SEED=$1
 
 echo "Training CIFAR100"
-python main.py --model-name="VGG16" --dataset-name="cifar100" --seed=655321
-python main.py --model-name="Conv6" --dataset-name="cifar100" --seed=655321
-python main.py --model-name="Conv2" --dataset-name="cifar100" --seed=655321
-python main.py --model-name="FC"    --dataset-name="cifar100" --seed=655321
+python main.py --model-name="VGG16" --dataset-name="cifar100" --seed=$SEED
+python main.py --model-name="Conv6" --dataset-name="cifar100" --seed=$SEED
+python main.py --model-name="Conv2" --dataset-name="cifar100" --seed=$SEED
+python main.py --model-name="FC"    --dataset-name="cifar100" --seed=$SEED
 
 echo "Training CIFAR10"
-python main.py --model-name="VGG16" --dataset-name="cifar10" --seed=655321
-python main.py --model-name="Conv6" --dataset-name="cifar10" --seed=655321
-python main.py --model-name="Conv2" --dataset-name="cifar10" --seed=655321
-python main.py --model-name="FC"    --dataset-name="cifar10" --seed=655321
+python main.py --model-name="VGG16" --dataset-name="cifar10" --seed=$SEED
+python main.py --model-name="Conv6" --dataset-name="cifar10" --seed=$SEED
+python main.py --model-name="Conv2" --dataset-name="cifar10" --seed=$SEED
+python main.py --model-name="FC"    --dataset-name="cifar10" --seed=$SEED
 
 echo "Training MNIST"
-python main.py --model-name="VGG16" --dataset-name="mnist" --seed=655321
-python main.py --model-name="Conv6" --dataset-name="mnist" --seed=655321
-python main.py --model-name="Conv2" --dataset-name="mnist" --seed=655321
-python main.py --model-name="FC"    --dataset-name="mnist" --seed=655321
+python main.py --model-name="VGG16" --dataset-name="mnist" --seed=$SEED
+python main.py --model-name="Conv6" --dataset-name="mnist" --seed=$SEED
+python main.py --model-name="Conv2" --dataset-name="mnist" --seed=$SEED
+python main.py --model-name="FC"    --dataset-name="mnist" --seed=$SEED
 
 tar -zcf $SEED.tar.gz models/*$SEED
